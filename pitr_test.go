34038b0198069e349e32ad7c306b01f42e191db8
Kyungtae Lee <mlogue@blocko.io>
2018-04-17 13:57:49 +0900

    Move a pitr example to testcase

--- pitr_test.go
+++ pitr_test.go
@@ -1,9 +1,8 @@
-package main
+package sqlite3_test
 
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"os"
 
 	sqlite3 "bitbucket.org/cloudwallet/go-sqlite3"
@@ -15,7 +14,13 @@ const (
 	dataSrc  = "file:./foo.db?pitr=on&pitr_path=" + pitrPath
 )
 
-func main() {
+func checkErr(err error) {
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func ExamplePitr() {
 	_ = os.Remove("./foo.db")
 	_ = os.RemoveAll(pitrPath)
 
@@ -29,95 +34,98 @@ func main() {
 		},
 	})
 	db, err := sql.Open(driver, dataSrc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
 
 	err = db.Ping()
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
 
 	fmt.Printf("[%s]\n", sqlite3conn.GetLastRecoveryPoint())
 
 	_, err = db.Exec("create table foo (id integer not null primary key, name text)")
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
 
 	tx, _ := db.Begin()
-	tx.Exec("insert into foo(name) values ('ktlee'), ('sjwoo')")
+	_, err = tx.Exec("insert into foo(name) values ('ktlee'), ('sjwoo')")
+	checkErr(err)
 	tx.Commit()
 
 	point := sqlite3conn.GetLastRecoveryPoint()
 	fmt.Printf("[%s]\n", point)
 
 	tx, _ = db.Begin()
-	tx.Exec("insert into foo(name) values ('kslee'), ('hypark')")
+	_, err = tx.Exec("insert into foo(name) values ('kslee'), ('hypark')")
+	checkErr(err)
 	tx.Commit()
 
 	tx, _ = db.Begin()
-	//tx.Exec("insert into foo(name) values ('hypark')")
-	tx.Exec("update foo set name = 'mlogue' where id = 1")
+	_, err = tx.Exec("update foo set name = 'mlogue' where id = 1")
+	checkErr(err)
 	tx.Commit()
 
 	fmt.Printf("[%s]\n", sqlite3conn.GetLastRecoveryPoint())
 
 	var name string
 	rows, err := db.Query("select name from foo order by id")
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkErr(err)
 	for rows.Next() {
 		err = rows.Scan(&name)
-		if err != nil {
-			fmt.Println(err)
-		}
+		checkErr(err)
 		fmt.Println(name)
 	}
 	err = rows.Err()
-	if err != nil {
-		fmt.Println(err)
-	}
-	rows.Close()
+	checkErr(err)
+
+	err = rows.Close()
+	checkErr(err)
 
 	count := sqlite3conn.RestoreToPoint(point)
 	fmt.Printf("restore to %s, count: %d\n", point, count)
 
 	row := db.QueryRow("select name from foo where id = 4")
 	err = row.Scan(&name)
-	if err != nil {
-		fmt.Println(err)
-	} else {
+	checkErr(err)
+	if err == nil {
 		fmt.Println(name)
 	}
 
 	row = db.QueryRow("select name from foo where id = 1")
 	err = row.Scan(&name)
-	if err != nil {
-		fmt.Println(err)
-	} else {
+	checkErr(err)
+	if err == nil {
 		fmt.Println(name)
 	}
 
-	db.Close()
+	err = db.Close()
+	checkErr(err)
 
 	fmt.Println("opening new db")
 	db, err = sql.Open(driver, dataSrc)
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
 
-	db.Ping()
+	err = db.Ping()
+	checkErr(err)
 
 	tx, _ = db.Begin()
-	tx.Exec("insert into foo(name) values ('kslee'), ('hypark')")
+	_, err = tx.Exec("insert into foo(name) values ('kslee'), ('hypark')")
+	checkErr(err)
 	tx.Commit()
 
 	fmt.Printf("[%s]\n", sqlite3conn.GetLastRecoveryPoint())
 
 	err = db.Close()
-	if err != nil {
-		fmt.Println(err)
-	}
+	checkErr(err)
+
+	// Output:
+	// []
+	// [foo.db.0000000000000002]
+	// [foo.db.0000000000000004]
+	// mlogue
+	// sjwoo
+	// kslee
+	// hypark
+	// restore to foo.db.0000000000000002, count: 2
+	// sql: no rows in result set
+	// ktlee
+	// opening new db
+	// [foo.db.0000000000000003]
 }
